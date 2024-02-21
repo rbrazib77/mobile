9d227ceb806a447058f4993289c1b56d9f09df40
rbrazib77 <rbrazib77@gmail.com>
2024-02-21 15:00:02 +0600

    first commit

---
+++ assets/js/pipup.js
@@ -0,0 +1,100 @@
+/* magnificPopup img view */
+$('.popup-image-1').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-2').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-3').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-4').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-5').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-6').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-7').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-8').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+
+/* magnificPopup img view */
+$('.popup-image-9').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-10').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-11').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-12').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-13').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-14').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-15').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+$('.popup-image-16').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+});
+
